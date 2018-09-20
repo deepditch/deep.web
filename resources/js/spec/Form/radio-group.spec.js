a5566f46f6c6ab501d06f0267e4370bd5fb74ac2
drakesvoboda <drakesvoboda@gmail.com>
2018-09-20 16:48:48 -0400

    Added testing with Jest. Currently broken

---
+++ resources/js/spec/Form/radio-group.spec.js
@@ -0,0 +1,15 @@
+import { RadioGroup, RadioGroupOption } from 'components/Form/radio-group';
+
+describe('RadioGroup', () => {
+  it('should render without crashing', () =>{
+    const div = document.createElement('div');
+    ReactDOM.render(<RadioGroup/>, div);
+  });
+
+  describe('RadioGroupOption', () => {
+    it('should render without crashing', () =>{
+      const div = document.createElement('div');
+      ReactDOM.render(<RadioGroupOption/>, div);
+    });
+  });
+})
