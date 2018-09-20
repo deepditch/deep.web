28753224f20be3037cde5e69f97ca4070794975d
drakesvoboda <drakesvoboda@gmail.com>
2018-09-20 13:01:17 -0400

    Fix style links in we.blade.php. Fix image link

--- resources/views/web.blade.php
+++ resources/views/web.blade.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/vendor.css')}}">
-        <link rel="stylesheet" href="{{ asset('css/main.css')}}">
+        <link rel="stylesheet" href="{{ asset('styles/vendor.css')}}">
+        <link rel="stylesheet" href="{{ asset('styles/main.css')}}">
     </head>
     <body>
         <div id="react-container"></div>
