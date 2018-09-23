c1a256cfb2eaae224fb7be448012ba8a5dd49380
Hassan Bazzi <hbazzi@nexcess.net>
2018-09-23 12:25:05 -0400

    more cleaning

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -43,12 +43,12 @@ class Handler extends ExceptionHandler
     }
 
     /**
-    * Render an exception into an HTTP response.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @param  \Exception  $exception
-    * @return \Illuminate\Http\Response
-    */
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
     public function render($request, Exception $exception)
     {
         // detect instance
