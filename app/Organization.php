6a3473aea857b983c421b3aaa131a076c9c64ba2
Hassan Bazzi <hbazzi@nexcess.net>
2018-09-24 23:31:24 -0400

    started implementing organization

---
+++ app/Organization.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organization extends Model
+{
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+    protected $fillable = [
+    'name',
+    'address',
+    'city',
+    'state',
+    'zip',
+  ];
+
+}
